5f73c96c91b729a1d90a795756ef84ede9c3c198
João Romão <jrmromao@gmail.com>
2024-06-29 11:20:17 +0100

    Remove unnecessary empty lines and comments in create function.
    Removed redundant empty lines and comments from the create function to improve code readability.

--- lib/actions/licenseTool.actions.ts
+++ lib/actions/licenseTool.actions.ts
@@ -27,10 +27,6 @@ export const create = async (data: {
         })
 
 
-
-
-
-        
         // id             Int          @id @default(autoincrement())
         // action         String
         // entity         String
