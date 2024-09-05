41d2bad327409d02a41b12086f28d78c815cfd2c
João Romão <jrmromao@gmail.com>
2024-09-05 09:05:50 +0100

    Add Google Analytics

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
+import Script from "next/script";
 
 
 export default async function RootLayout({
@@ -12,6 +13,18 @@ export default async function RootLayout({
 
     return (
         <main className="flex min-h-screen w-full font-inter">
+
+            <header>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-K3TNF3MCG4"></Script>
+                <Script id={'google-analytics'}>
+                    {`window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-K3TNF3MCG4');
+                    `}
+                </Script>
+            </header>
+
             <Sidebar key={'jhvbjhvjhvbj'}/>
             <div className="flex size-full flex-col">
                 <div className="root-layout">
