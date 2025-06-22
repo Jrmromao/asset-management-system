73184a7498de9753ecaad0821a7f132ea7e69158
João Filipe Romão <jrmromao@gmail.com>
2025-06-22 08:18:20 +0100

    Enhances header icon with conditional link and style
    Introduces conditional routing for header icon link to toggle
    between admin and root paths based on a condition. Improves
    styling by updating the icon and text to align with the
    application's design standards, using Leaf icon and updated
    font styles.
    
    Refactors component to enhance readability and maintainability.

--- components/page/HeaderIcon.tsx
+++ components/page/HeaderIcon.tsx
@@ -3,20 +3,40 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { Leaf } from "lucide-react";
+import { APP_NAME } from "@/constants";
 
 const HeaderIcon = ({ isCollapsed = false }: { isCollapsed?: boolean }) => {
   return (
-    <Link href="/" className="flex items-center gap-2">
-      <Image src="/icons/logo.svg" width={34} height={34} alt="Horizon logo" />
-      <h1
-        className={cn("text-26 font-ibm-plex-serif font-bold text-black-1", {
+    // <Link href="/" className="flex items-center gap-2">
+    //   <Image src="/icons/logo.svg" width={34} height={34} alt="Horizon logo" />
+    //   <h1
+      //   className={cn("text-26 font-ibm-plex-serif font-bold text-black-1", {
+      //     hidden: isCollapsed,
+      //   })}
+      // >
+    //     EcoKeepr
+    //   </h1>
+    // </Link>
+
+
+<Link
+href={true ? "/admin" : "/"}
+className="flex items-center gap-2.5 hover:opacity-90 transition-opacity"
+>
+<Leaf className="w-7 h-7 text-emerald-600" />
+<div className="flex flex-col">
+  <span  className={cn("text-26 font-ibm-plex-serif font-bold text-black-1", {
           hidden: isCollapsed,
         })}
       >
-        EcoKeepr
-      </h1>
-    </Link>
+    {APP_NAME}</span>
+</div>
+</Link>
   );
 };
 
 export default HeaderIcon;
+
+
+
